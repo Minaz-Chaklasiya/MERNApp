547406e50a56bddc3fd0747a02b6196c8414dfaa
Minaz-Chaklasiya <minaz.chaklasiya@bacancytechnology.com>
2019-04-19 12:43:09 +0530

    mongouri

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI:"mongodb://localhost:27017/"
+    // mongoURI:"mongodb://localhost:27017/"
+    mongoURI:"mongodb+srv://dbShopping:dbShopping@cluster0-7mplf.mongodb.net/test?retryWrites=true"
 }
