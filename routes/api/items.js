f82ff3db8cb4fc7ad0d5c740da6b8e250157c8d4
Minaz-Chaklasiya <minaz.chaklasiya@bacancytechnology.com>
2019-04-19 10:52:33 +0530

    initial commit

---
+++ routes/api/items.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const router=express.Router();
+
+//Item model
+const Item=require('../../models/Item');
+
+//@route Get api.items
+//@desc Get All Items
+//@access public
+
+router.get('/',(req,res)=>{
+    Item.find()
+        .sort({date:-1})
+        .then(items=>res.json(items))
+});
+
+//@route Post api/items
+//@desc Create A Post
+//@access  Public
+router.post('/',(req,res)=>{
+    const newItem=new Item({
+        name:req.body.name
+    });
+
+    newItem.save().then(item=>res.json(item));
+})
+
+//@route Delete api/items/:id
+//@desc Delete an item
+//@access public
+router.delete('/:id',(req,res)=>{
+    Item.findById(req.params.id)
+        .then(item=>item.remove().then(()=>res.json({success:true})))
+        .catch(err=>res.status(404).json({success:false}));
+})
+
+module.exports=router;
